bc7c49f5b5f098a0a2698d89b9aec516d4216ccd
Facundo W <99218221+CPetruzzo@users.noreply.github.com>
2024-06-19 10:21:38 -0300

    minor changes on logs and first time use warning

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <h1>Where knowledge begins</h1>
         <ChatInterface />
+        <p>First time you enter it may take a while (up to 50 seconds) since it's deployed on Render (with a free account).</p>
       </header>
     </div>
   );
